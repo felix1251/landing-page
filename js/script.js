9f6346c80f6838b7a7e0f656f0932c2414e8f7ea
felix1251 <domhack123@gmail.com>
2022-12-13 16:06:56 +0800

    hide mobile in desktop view

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+if(!localStorage.getItem("theme")){
+	localStorage.setItem("theme", "light")
+}
+
+// window.addEventListener("resize", (e) => {
+// 	if(document.getElementById("mobile-nav").offsetWidth > 0 && document.getElementsByTagName("body")[0].offsetWidth >= 1100){
+// 		document.getElementById("mobile-nav").style.width = "0"
+// 	}
+// });
+
 function closeNav() {
 	document.getElementById("mobile-nav").style.width = "0";
 }
